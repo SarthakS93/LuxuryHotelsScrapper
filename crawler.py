a0da312ce6406b3b7eebe00bb32aae27766a3c27
sarthak <sarthaks93@gmail.com>
2016-08-15 19:54:23 +0530

    getBasicInfo: complete

--- crawler.py
+++ crawler.py
@@ -6,7 +6,48 @@ from urllib.parse import urljoin
 
 url = 'http://www.fivestaralliance.com/luxury-hotels/bangkok/banyan-tree-bangkok'
 
+info = {}
+
+def getBasicInfo(soup):
+    print("Getting Basic Info")
+    name_tag = soup.find(id = 'heading_title_hotel')
+    info['name'] = name_tag.text
+    location_tag = soup.find(id = 'title_location')
+    info['location'] = location_tag.text
+
+def connect(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    else:
+        soup = BeautifulSoup(r.text)
+        return soup
+
+
+def scrape(soup):
+    getBasicInfo(soup)
+    #getOverView(soup)
+    #getLocation(soup)
+    #getReviews(soup)
+    #getAmenities(soup)
+    #getAwards(soup)
+    showOutput()
+
+
+def showOutput():
+    print("**Output is as follows**")
+    print(info)
+
+def crawl(url):
+    print("Crawling this page: ", url)
+    soup = connect(url)
+    if soup == None:
+        print("Exception")
+        return
+    else:
+        scrape(soup)
 
 
 if __name__ == '__main__':
-    print(url)
+    #print(url)
+    crawl(url)
