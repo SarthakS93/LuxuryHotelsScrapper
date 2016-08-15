678f4953078ae948a39d1e83eb4fb4ae6cf8ed43
sarthak <sarthaks93@gmail.com>
2016-08-15 19:32:28 +0530

    initialized crawler module

---
+++ crawler.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import sys
+from urllib.parse import urljoin
+
+url = 'http://www.fivestaralliance.com/luxury-hotels/bangkok/banyan-tree-bangkok'
+
+
+
+if __name__ == '__main__':
+    print(url)
