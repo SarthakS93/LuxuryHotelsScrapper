b331887a9e4783970f81fe3627c9565d7efa94b4
sarthak <sarthaks93@gmail.com>
2016-09-20 12:20:59 +0530

    check complete

---
+++ checkIfAllImagesScrapped.py
@@ -0,0 +1,33 @@
+import os, csv
+
+def check(filename):
+    print('Inside check')
+
+    folders_list = os.listdir('pics/')
+
+    file = open(filename, 'r')
+    reader = csv.reader(file)
+
+    data_list = list(reader)
+
+    for i in data_list:
+        name = i[0]
+        location = i[1]
+
+        temp = 0
+
+        for j in folders_list:
+            if name in j and location in j:
+                temp = 1
+                break
+
+        if temp == 0:
+            print('$$$$', name, location)
+
+
+if __name__ == '__main__':
+    print('Starting')
+    check('thailand.csv')
+    print('End')
+
+
